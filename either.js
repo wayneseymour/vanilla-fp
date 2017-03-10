75686bfff919685436334bd34ac4aa04b2b28529
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2017-03-10 11:11:22 -0500

    More Brian Losdorf-ian functional composition w/o duplication...I think :)

--- either.js
+++ either.js
@@ -1,26 +1,26 @@
 const log = console.log.bind(console, 'Result:')
-
-// const Either = Right || Left
-
 const Right = x =>
   ({
     map: f => Right(f(x)),
     fold: (f, g) => g(x),
     inspect: () => `Right(${x})`
   })
-
 const Left = x =>
   ({
     map: f => Left(x),
     fold: (f, g) => f(x),
     inspect: () => `Left(${x})`
   })
+const findColor = name => {
+  const found = ({red: '#red', blue: '#blue', yellow: '#yellow'})[name]
+  return found ? Right(found) : Left(null)
+}
+const findGreen = findColor.bind(null, 'green')
+const findRed = findColor.bind(null, 'red')
+const end = v => v
+  .map(c => c.slice(1))
+  .fold(e => 'no color',
+    c => c.toUpperCase()
+  )
+log(end(findGreen()), end(findRed()))
 
-const result =
-  // Right(2) // Folds out the value
-  Left(2)     // Folds out the 'error'
-    .map(x => x + 1)
-    .map(x => x / 2)
-    .fold(x => 'error', x => x)
-
-log(result)
